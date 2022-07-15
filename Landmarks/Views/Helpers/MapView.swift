87a76017912cc52e15d475732f0f071971967464
Jiaming Guo <j23guo@gmail.com>
2022-07-15 00:04:17 -0400

    added preferences for map zoom in mac version

--- Landmarks/Views/Helpers/MapView.swift
+++ Landmarks/Views/Helpers/MapView.swift
@@ -9,18 +9,38 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion()
     var coordinate: CLLocationCoordinate2D
     
-    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
-        region = MKCoordinateRegion(
-            center: coordinate,
-            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-        )
+    @AppStorage("MapView.zoom")
+    private var zoom: Zoom = .medium
+    
+    enum Zoom: String, CaseIterable, Identifiable {
+        case near = "Near"
+        case medium = "Medium"
+        case far = "Far"
+
+        var id: Zoom {
+            return self
+        }
+    }
+    
+    var delta: CLLocationDegrees {
+        switch zoom {
+            case .near: return 0.02
+            case .medium: return 0.2
+            case .far: return 2
+        }
     }
     
     var body: some View {
-        Map(coordinateRegion: $region)
+            Map(coordinateRegion: .constant(region))
+        }
+
+    var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
+        )
     }
 }
 
